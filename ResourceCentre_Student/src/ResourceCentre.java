2fe52e2414d14dab80db29c596c3e95b1ce547d2
20045485 <20045485@myrp.edu.sg>
2021-07-22 16:42:54 +0800

    done by gilbert

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -246,7 +246,7 @@ public class ResourceCentre {
 		return isLoaned;
 	}
 	public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
-		// write your code here gilbertngyk1234
+		// write your code here gilbertngyk
 		ResourceCentre.viewAllChromebook(chromebookList);
 		String tag = Helper.readString("Enter asset tag > ");
 		String due = Helper.readString("Enter due date > ");
